45f6ee5e9cd1a81b409fd5571672b20b50e058fc
Julien <julien.denize@telecom-sudparis.eu>
2018-03-03 21:11:38 +0100

    avancée dans l'implémentation de Condorcet

--- Java/Algortihme de Condorcet/src/Condorcet.java
+++ Java/Algortihme de Condorcet/src/Condorcet.java
@@ -5,30 +5,44 @@ public class Condorcet {
 	private LinkedList<Voix> toutesLesVoix;
 	private int[][] compteur;
 	private int gagnant;
+	
 	public Condorcet(int nbAlternatives) {
 		this.nbAlternatives = nbAlternatives;
 		this.compteur = new int[nbAlternatives][nbAlternatives];
 		this.gagnant = 0;
 		this.toutesLesVoix = new LinkedList<Voix>();
 	}
+	
 	public void addVoix(Voix voix) {
 		this.toutesLesVoix.add(voix);
 	}
+	
 	public void parcoursVoix() {
 		for (Voix cur: toutesLesVoix) {
 			for (int i = 0; i < nbAlternatives; i++) {
 				if (cur.rangAlternative(i) != 0) {
 					for (int j = i+1; j < nbAlternatives; j++) {
 						if (cur.rangAlternative(j) != 0) {
-							compteur[i][j] += 1;
+							if (cur.rangAlternative(i) < cur.rangAlternative(j)) compteur[j][i] += 1;
+							else compteur[i][j] +=1;
 						}
 					}
 				}
 			}
 		}
 	}
+	
+	public void lectureCompteur() {
+		for (int i = 0; i < nbAlternatives; i++) {
+			for (int j = 0; j < nbAlternatives; j++) {
+				System.out.print(Integer.toString(compteur[i][j]) + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+	
 	public String toString() {
-		return "Election avec " + Integer.toString(nbAlternatives) + " alternatives et le gagnat est " + Integer.toString(gagnant);
+		return "Election avec " + Integer.toString(nbAlternatives) + " alternatives et le gagnant est " + Integer.toString(gagnant);
 	}
 
 }
