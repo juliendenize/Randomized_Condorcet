3e7f446908df6ce6f750351b2fd68dddd3488ece
Estelle Canovas <canova_e@telecom-sudparis.eu>
2018-03-15 18:20:57 +0100

    ajout template provisoire site web

---
+++ Site web/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+</head>
+<body>
+	<header>
+	<h1>Plateforme de vote selon la méthode de Condorcet randomisé</h1>
+		<a href="inscription.php">Inscription</a>
+		<a href="connexion.php">Se connecter</a>
+	</header>
+
+	<nav>
+		<a href="newvote.php" title="Créer un nouveau vote">Nouveau vote</a>
+		<a href="running_votes.php" title="Votes en cours">Votes en cours</a>
+	</nav>
+
+	<section>
+		<h1>La méthode de Condorcet, qu'est-ce que c'est ?</h1>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/wKimU8jy2a8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+	</section>
+
+	<footer>
+		<a href="mailto:estelle.canovas@telecom-sudparis.eu">Nous contacter</a>
+	</footer>
+
+</body>
+</html>
