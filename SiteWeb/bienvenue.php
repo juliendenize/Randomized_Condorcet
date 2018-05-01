2ef69a8da786233be367ac514d01310124609db7
Julien <julien.denize@telecom-sudparis.eu>
2018-05-01 12:37:29 +0200

    Début branche pour mettre en place la structure mvc

---
+++ SiteWeb/bienvenue.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+	<header>
+	<h1>Plateforme de vote selon la méthode de Condorcet randomisé</h1>
+		<a href="inscription.php" class="btn btn-default">Inscription</a>
+		<a href="connexion.php">Se connecter</a>
+	</header>
+
+	<nav>
+		<a href="new_vote_nb.php" title="Créer un nouveau vote">Nouveau vote</a>
+		<a href="running_votes.php" title="Votes en cours">Votes en cours</a>
+	</nav>
+
+	<section>
+		<h2>La méthode de Condorcet, qu'est-ce que c'est ?</h2>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/wKimU8jy2a8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+	</section>
+
+	<footer>
+		<a href="mailto:estelle.canovas@telecom-sudparis.eu">Nous contacter</a>
+	</footer>
+
+</body>
+</html>
